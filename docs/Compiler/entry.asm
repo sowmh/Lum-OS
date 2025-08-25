bd33145dc8961d4f263edb38c7838092f602c1d4
Lum <swmoha69@gmail.com>
2025-08-25 15:10:53 +0200

    Update entry.asm

--- docs/Compiler/entry.asm
+++ docs/Compiler/entry.asm
@@ -5,6 +5,6 @@ section .text
 global _start
 
 _start:
-    mov rax, 60     ; syscall número 60 = exit
-    xor rdi, rdi    ; código de salida 0
+    mov rax, 60     
+    xor rdi, rdi    
     syscall
