354bd52a492faaf73d556b7f5e5c55aab5cec75e
Lum <swmoha69@gmail.com>
2026-04-07 20:10:45 +0200

    Implement entry point and stack setup in crt0.asm
    Initialize segment registers and setup stack for Stage 2 bootloader. Pass boot drive to C function.

--- src/bootloader/stage2/crt0.asm
+++ src/bootloader/stage2/crt0.asm
@@ -1,6 +1,35 @@
+; src/bootloader/stage2/crt0.asm
 BITS 16
-global _cstart_
 
-_cstart_:
+section _ENTRY class=CODE
+
+extern _cstart_
+global entry
+
+entry:
+    cli                         ; Desactiva interrupciones durante el setup
     
-    ret
+    ; Stage 2 cargado en 0x2000:0x0000
+    mov ax, 0x2000
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    ; Setup del Stack (0x2000:0xFFF0) - Lejos del Stage 1 (0x7C00)
+    mov ss, ax
+    mov sp, 0xFFF0
+    mov bp, sp
+    
+    sti                         ; Reactiva interrupciones
+    
+    ; Pasar el drive de booteo (DL) a la función C
+    xor dh, dh
+    push dx
+    
+    call _cstart_               ; Salto al main.c
+    
+.hang:
+    cli
+    hlt
+    jmp .hang
