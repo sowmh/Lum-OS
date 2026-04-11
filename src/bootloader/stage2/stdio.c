ff73a2ae89569ef421c9f6411a82114aa70b9422
Lum <swmoha69@gmail.com>
2026-04-11 23:28:18 +0200

    Update stdio.c

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -1,8 +1,7 @@
+// src/bootloader/stage2/stdio.c
 #include "stdio.h"
 #include "stdint.h"
-
-// Enlaza con la función de x86.asm que usa la BIOS
-extern void _cdecl x86_Video_WriteCharTeletype(char c, uint8_t page);
+#include "x86.h"
 
 void _cdecl putc(char c) {
     if (c == '\n') {
@@ -15,28 +14,95 @@ void _cdecl putc(char c) {
 
 void _cdecl puts(const char* str) {
     while (*str) {
-        putc(*str++);
+        putc(*str);
+        str++;
     }
 }
 
-// Auxiliar para imprimir números en hexadecimal
-static void print_hex(uint32_t num) {
-    char* hex_chars = "0123456789ABCDEF";
+void _cdecl clrscr(void) {
+    x86_Video_SetMode(0x03); // 80x25 text mode
+}
+
+// Helper functions
+static char hex_digit(uint8_t val) {
+    return val < 10 ? '0' + val : 'A' + (val - 10);
+}
+
+static void print_hex16(uint16_t num) {
+    putc('0'); putc('x');
+    for (int i = 3; i >= 0; i--) {
+        putc(hex_digit((num >> (i * 4)) & 0xF));
+    }
+}
+
+static void print_hex32(uint32_t num) {
     putc('0'); putc('x');
     for (int i = 7; i >= 0; i--) {
-        putc(hex_chars[(num >> (i * 4)) & 0xF]);
+        putc(hex_digit((num >> (i * 4)) & 0xF));
+    }
+}
+
+static void print_dec(uint32_t num) {
+    char buf[12];
+    int i = 0;
+    
+    if (num == 0) {
+        putc('0');
+        return;
+    }
+    
+    while (num > 0) {
+        buf[i++] = '0' + (num % 10);
+        num /= 10;
+    }
+    
+    while (i > 0) {
+        putc(buf[--i]);
     }
 }
 
 void _cdecl printf(const char* fmt, ...) {
-    uint16_t* args = (uint16_t*)&fmt + 1; // Puntero a argumentos en el stack
+    uint16_t* args = (uint16_t*)&fmt;
+    args++; // Skip format string pointer
+    
     while (*fmt) {
         if (*fmt == '%' && *(fmt + 1)) {
             fmt++;
             switch (*fmt) {
-                case 's': puts(*(char**)args); args += 2; break;
-                case 'x': print_hex(*(uint32_t*)args); args += 2; break;
-                case 'd': /* implementar print_dec si es necesario */ break;
+                case 's': {
+                    const char* str = (const char*)(*args);
+                    args++;
+                    if (str) puts(str);
+                    break;
+                }
+                case 'c': {
+                    char c = (char)(*args);
+                    args++;
+                    putc(c);
+                    break;
+                }
+                case 'd': {
+                    uint16_t num = *args;
+                    args++;
+                    print_dec(num);
+                    break;
+                }
+                case 'x': {
+                    uint16_t num = *args;
+                    args++;
+                    print_hex16(num);
+                    break;
+                }
+                case 'p': {
+                    uint32_t ptr = *(uint32_t*)args;
+                    args += 2;
+                    print_hex32(ptr);
+                    break;
+                }
+                case '%': {
+                    putc('%');
+                    break;
+                }
             }
         } else {
             putc(*fmt);
