d435141412755551bf48627e421d2662dd54715c
Lum <swmoha69@gmail.com>
2026-04-11 23:27:37 +0200

    Add cdecl calling convention to stdio functions

--- src/bootloader/stage2/stdio.h
+++ src/bootloader/stage2/stdio.h
@@ -1,7 +1,7 @@
+// src/bootloader/stage2/stdio.h
 #pragma once
 
-
-void putc(char c);
-
-
-void puts(const char* str);
+void _cdecl putc(char c);
+void _cdecl puts(const char* str);
+void _cdecl printf(const char* fmt, ...);
+void _cdecl clrscr(void);
